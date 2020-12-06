c80ec52ff0892a854ddf947b78d98d621d4e87c4
Utsav Kumar <66728108+youteeeee@users.noreply.github.com>
2020-12-06 21:58:39 +0530

    feat: added textarea

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var btnTranslate= document.querySelector("#btn-translate");
-
-btnTranslate.addEventListener("click",()=>{
+var txtInput=document.querySelector("#txt-input");
+ 
+const clickHandler = ()=>{
   console.log("clicked!!");
-});
+  console.log('value is=',txtInput.value);
+
+};
+
+btnTranslate.addEventListener("click",clickHandler);
